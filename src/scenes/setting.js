ba71e077f828e1374f9f0ba2d0d7d750ffb138cc
Emma Li <Ema@emmas-mbp.dynamic.ucsd.edu>
2016-05-20 12:44:06 -0700

    change password and email

--- src/scenes/setting.js
+++ src/scenes/setting.js
@@ -5,6 +5,7 @@ import React, {
   Text,
   TouchableHighlight,
   StyleSheet,
+  TextInput,
   Image,
   AsyncStorage,
   View
@@ -34,6 +35,44 @@ class Setting extends Component {
       });
   }
 
+  logoutbutton(){
+    return (
+      <TouchableHighlight onPress = {this.logout.bind(this)} underlayColor='lemonchiffon'>
+        <Text style = {{color: 'black',
+        fontSize: 16,
+        textAlign: 'center'}}>
+          logout
+        </Text>
+      </TouchableHighlight>
+    );
+  }
+  /*Todo*/
+  changePassword(){
+  }
+
+  changeEmail(){
+    database.changeEmail({
+      oldEmail: this.state.user.password.email,
+      newEmail: "email@mail.com",
+      password: "t"
+    }, function(error) {
+    if (error) {
+      switch (error.code) {
+        case "INVALID_PASSWORD":
+          alert ("The specified user account password is incorrect.");
+          break;
+        case "INVALID_USER":
+          alert ("The specified user account does not exist.");
+          break;
+        default:
+          alert ("Error creating user:", error);
+      }
+    } else {
+        alert("User email changed successfully!");
+      }
+    });
+  }
+
   render() {
     AsyncStorage.getItem('user_data').then((user_data_json) => {
       let user_data = JSON.parse(user_data_json);
@@ -57,6 +96,21 @@ class Setting extends Component {
               <View style={page_styles.email_container}>
                 <Text style={page_styles.email_text}>{this.state.user.password.email}
                 </Text>
+                <TextInput
+                  placeholder = {"email"}
+                  onChangeText = {(text) => this.setState({email: text})}
+                  value = {this.state.email}
+                  style = {SceneStyles.firstName}
+                  placeholderTextColor = 'black'
+                  underlineColorAndroid = 'white'
+                />
+                <TouchableHighlight onPress = {this.changeEmail.bind(this)} underlayColor='lemonchiffon'>
+                  <Text style = {{color: 'black',
+                  fontSize: 16,
+                  textAlign: 'center'}}>
+                    change email
+                  </Text>
+                </TouchableHighlight>
                 <Image
                   style={SceneStyles.image}
                   source={{uri: this.state.user.password.profileImageURL}}
@@ -64,13 +118,7 @@ class Setting extends Component {
                 <Text style={page_styles.email_text}>{this.state.user.token}
                 </Text>
               </View>
-                <TouchableHighlight onPress = {this.logout.bind(this)} underlayColor='lemonchiffon'>
-                  <Text style = {{color: 'black',
-                  fontSize: 16,
-                  textAlign: 'center'}}>
-                    logout
-                  </Text>
-                </TouchableHighlight>
+                {this.logoutbutton()}
             </View>
         }
         </View>
