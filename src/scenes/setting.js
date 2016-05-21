81929dc40ac39edfbd8786b7fa2b4a7a8dc656ae
Johj <peter.han.9493@gmail.com>
2016-05-21 15:29:24 -0700

    clean up settings.

--- src/scenes/setting.js
+++ src/scenes/setting.js
@@ -1,40 +1,36 @@
 'use strict';
 
 import React, {
+  AsyncStorage,
   Component,
-  Text,
-  TouchableHighlight,
   StyleSheet,
   Image,
-  AsyncStorage,
+  Text,
+  TouchableHighlight,
   View
 } from 'react-native';
 
 import Firebase from 'firebase';
-import SceneStyles from '../styles/scene-styles';
-import Header from '../components/header';
-import Button from '../components/button';
+
 import Login from './login';
 
-let database = new Firebase("poopapp1.firebaseio.com/users");
+import Header from '../components/header';
+import Button from '../components/button';
 
-class Setting extends Component {
+import SceneStyles from '../styles/scene-styles';
+import ButtonStyles from '../styles/button-styles';
 
-  constructor(props){
+let database = new Firebase("poopapp1.firebaseio.com");
+
+export default class Setting extends Component {
+  constructor(props) {
     super(props);
     this.state = {
       loaded: false,
     }
   }
 
-  logout(){
-      AsyncStorage.removeItem('user_data').then(() => {
-        database.unauth();
-        this.props.navigator.resetTo(this.refs.navigator.getCurrentRoutes()[0]);
-      });
-  }
-
-  render() {
+  componentWillMount() {
     AsyncStorage.getItem('user_data').then((user_data_json) => {
       let user_data = JSON.parse(user_data_json);
       this.setState({
@@ -42,6 +38,9 @@ class Setting extends Component {
         loaded: true
       });
     });
+  }
+
+  render() {
     return(
       <View>
         <Header
@@ -52,24 +51,24 @@ class Setting extends Component {
         <View>
         {
           this.state.user &&
-            <View style={SceneStyles.body}>
-              <View style={page_styles.email_container}>
-                <Text style={page_styles.email_text}>{this.state.user.password.email}
-                </Text>
-                <Image
-                  style={SceneStyles.image}
-                  source={{uri: this.state.user.password.profileImageURL}}
-               />
-                <Text style={page_styles.email_text}>{this.state.user.password.lastName}
-                </Text>
-              </View>
-                <TouchableHighlight onPress = {this.logout.bind(this)} underlayColor='lemonchiffon'>
-                  <Text style = {{color: 'black',
-                  fontSize: 16,
-                  textAlign: 'center'}}>
-                    logout
-                  </Text>
-                </TouchableHighlight>
+            <View style = {SceneStyles.body}>
+              <Text style = {styles.emailText}>
+                {this.state.user.password.email}
+              </Text>
+              <Image
+                style = {SceneStyles.image}
+                source = {{uri: this.state.user.password.profileImageURL}}
+              />
+              <Text style = {styles.emailText}>
+                {this.state.user.password.lastName}
+              </Text>
+              <Button
+                text = "LOG OUT"
+                onPress = {this.logout.bind(this)}
+                buttonStyles = {ButtonStyles.primaryButton}
+                buttonTextStyles = {ButtonStyles.primaryButtonText}
+                underlayColor = {"#B18C40"}
+              />
             </View>
         }
         </View>
@@ -77,13 +76,16 @@ class Setting extends Component {
     );
   }
 
+  logout() {
+      AsyncStorage.removeItem('user_data').then(() => {
+        database.unauth();
+        this.props.navigator.resetTo({component: Login});
+      });
+  }
 }
 
-const page_styles = StyleSheet.create({
-  email_container: {
-    padding: 20
-  },
-  email_text: {
+const styles = StyleSheet.create({
+  emailText: {
     fontSize: 18
   }
 });
