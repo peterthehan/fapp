436058e5d3331d383522f13dc270432b51cab06f
Andrew Han <andrewthehan@gmail.com>
2016-05-22 03:17:27 -0700

    Home view instantly updates new posts.

--- src/scenes/home.js
+++ src/scenes/home.js
@@ -19,25 +19,6 @@ import SmallPost from '../components/small-post';
 
 let database = new Firebase("poopapp1.firebaseio.com");
 
-const pictures = [
-  "https://pbs.twimg.com/profile_images/723442376933396481/V3QBgFkA.jpg",
-  "https://pbs.twimg.com/profile_images/597793076514426880/qka9dYR-_400x400.jpg",
-  "http://static.wixstatic.com/media/95a3cf_dc7f0c0841ed4228bc6c9a8937a9878e.jpg_256",
-  "http://mediad.publicbroadcasting.net/p/wamc/files/styles/medium/public/201401/Fruit_%26_vegs_assortment_0.jpg",
-  "https://pbs.twimg.com/profile_images/723442376933396481/V3QBgFkA.jpg",
-  "https://pbs.twimg.com/profile_images/597793076514426880/qka9dYR-_400x400.jpg",
-  "http://static.wixstatic.com/media/95a3cf_dc7f0c0841ed4228bc6c9a8937a9878e.jpg_256",
-  "http://mediad.publicbroadcasting.net/p/wamc/files/styles/medium/public/201401/Fruit_%26_vegs_assortment_0.jpg",
-  "https://pbs.twimg.com/profile_images/723442376933396481/V3QBgFkA.jpg",
-  "https://pbs.twimg.com/profile_images/597793076514426880/qka9dYR-_400x400.jpg",
-  "http://static.wixstatic.com/media/95a3cf_dc7f0c0841ed4228bc6c9a8937a9878e.jpg_256",
-  "http://mediad.publicbroadcasting.net/p/wamc/files/styles/medium/public/201401/Fruit_%26_vegs_assortment_0.jpg",
-  "https://pbs.twimg.com/profile_images/723442376933396481/V3QBgFkA.jpg",
-  "https://pbs.twimg.com/profile_images/597793076514426880/qka9dYR-_400x400.jpg",
-  "http://static.wixstatic.com/media/95a3cf_dc7f0c0841ed4228bc6c9a8937a9878e.jpg_256",
-  "http://mediad.publicbroadcasting.net/p/wamc/files/styles/medium/public/201401/Fruit_%26_vegs_assortment_0.jpg",
-];
-
 const windowSize = Dimensions.get('window');
 
 class Home extends Component {
@@ -49,7 +30,13 @@ class Home extends Component {
     };
   }
 
-  componentDidMount(){
+  renderRow(post) {
+    return (
+      <SmallPost navigator={this.props.navigator} id={post}/>
+    );
+  }
+
+  queryData(){
     var myBlob = [];
     var self = this;
 
@@ -63,17 +50,8 @@ class Home extends Component {
     });
   }
 
-  renderRow(post) {
-    return (
-      <SmallPost navigator={this.props.navigator} id={post}/>
-    );
-  }
-
-  queryData(){
-    alert("ASFD");
-  }
-
   render() {
+    this.queryData();
     return(
       <View style = {{flex: 1}}>
         <TitleBar
